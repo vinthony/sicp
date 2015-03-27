890fefc0a85130b0f16322560386bd832ebf7552
南图 <vinthony@gmail.com>
2015-03-27 14:06:47 +0800

    modify 1.37

--- chapter 1/1.37.scm
+++ chapter 1/1.37.scm
@@ -1,9 +1,17 @@
 #lang planet neil/sicp
 ;;递归版本
+
+;;从小的加到大的
 (define (cont-frac f g k)
  (if (= k 0) (/ (f k) (g k)) (/ (f k) (+ (g k) (cont-frac f g (- k 1))))
  )
 )
+(define (c-f f g k)
+  (define (cf i)
+    (if (= i k) (/ (f k) (g k)) (/ (f k) (+ (g k) (cf (+ i 1)))))
+    )
+  (cf 1)
+  )
 
 (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 100)
 
@@ -16,3 +24,4 @@
   (iter k (/ (f k) (g k)))
   )
 (cont-frac-d (lambda (i) 1.0) (lambda (i) 1.0) 100)
+(c-f  (lambda (i) 1.0) (lambda (i) 1.0) 100)
